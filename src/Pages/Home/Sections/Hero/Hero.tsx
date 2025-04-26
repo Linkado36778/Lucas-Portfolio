027c66c4eab9aa6ce1ae1ae9ee6a1f81e16c73f7
Linkado36778 <barbosaa_lucas@hotmail.com>
2025-04-26 12:42:25 -0300

    refactor: added profile picture

--- src/Pages/Home/Sections/Hero/Hero.tsx
+++ src/Pages/Home/Sections/Hero/Hero.tsx
@@ -1,15 +1,41 @@
 import { styled } from "@mui/system"
+import { Grid } from "@mui/system"
+import Avatar from "../../../../assets/images/Lucas_profilePicture.jpeg"
 
 const Hero = () => {
 
     const StyledHero = styled("div")(() => ({
-        backgroundColor: "black"
+        backgroundColor: "black",
+        color: "white"
+    }))
+
+    const StyledPfp = styled("img")(() => ({
+        width: "20%",
+        borderRadius: "70%",
+        borderColor: "black"
     }))
 
     return (
       <>
+        <StyledPfp src={Avatar}></StyledPfp>
+        
         <StyledHero>
-            Hello World
+
+          <Grid container spacing={2}>
+            <Grid size={8}>
+              size=8
+            </Grid>
+            <Grid size={4}>
+              size=4
+            </Grid>
+            <Grid size={4}>
+              size=4
+            </Grid>
+            <Grid size={8}>
+              size=8
+            </Grid>
+          </Grid>
+        
         </StyledHero>
       </>
     )
